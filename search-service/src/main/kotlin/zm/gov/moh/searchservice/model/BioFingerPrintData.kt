f56ed799034c7a05483499bd6e58ef253b0204ca
Yengwe Tepula <lawrencetepula@gmail.com>
2023-11-07 10:58:22 +0200

    style: make data classes look better

--- search-service/src/main/kotlin/zm/gov/moh/searchservice/model/BioFingerPrintData.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/model/BioFingerPrintData.kt
@@ -1,5 +1,11 @@
 package zm.gov.moh.searchservice.model
 
+import com.machinezoo.sourceafis.FingerprintTemplate
+import zm.gov.moh.searchservice.utils.Constants
 import java.util.UUID
 
-data class BioFingerPrintData(val subjectId: UUID, val pos: String, val data: String)
+data class BioFingerPrintData(
+    val subjectId: UUID? = Constants.UNASSIGNED_UUID,
+    val pos: String? = "",
+    val data: FingerprintTemplate? = null
+)
--- search-service/src/main/kotlin/zm/gov/moh/searchservice/model/Subject.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/model/Subject.kt
@@ -3,4 +3,11 @@ package zm.gov.moh.searchservice.model
 import java.time.LocalDate
 import java.util.UUID
 
-class Subject(val id: UUID, val firstName: String, val lastName: String, val sex: Char, val dateOfBirth: LocalDate, val bioFingerprints: List<BioFingerPrintData>)
+class Subject(
+    val id: UUID,
+    val firstName: String,
+    val lastName: String,
+    val sex: Char,
+    val dateOfBirth: LocalDate,
+    val bioFingerprints: List<BioFingerPrintData>
+)
