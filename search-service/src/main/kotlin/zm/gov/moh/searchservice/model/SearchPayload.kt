f78b78ba3009f72d8f1db87617001e8c4c1bd3bb
Yengwe Tepula <lawrencetepula@gmail.com>
2023-11-07 12:22:37 +0200

    chore: change parameter name to probe

--- search-service/src/main/kotlin/zm/gov/moh/searchservice/model/SearchPayload.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/model/SearchPayload.kt
@@ -3,5 +3,5 @@ package zm.gov.moh.searchservice.model
 import com.machinezoo.sourceafis.FingerprintTemplate
 
 data class SearchPayload(
-    val fingerPrintTemplate: FingerprintTemplate? = null
+    val probe: FingerprintTemplate? = null
 )
