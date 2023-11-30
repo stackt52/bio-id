2f674fb2135d02eb2c90d716b38dca4f314a45c3
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-11-30 16:15:39 +0200

    Changed Redis operations to hashmap

--- bio-data-service/src/main/kotlin/zm/gov/moh/biodataservice/config/RedisConfig.kt
+++ bio-data-service/src/main/kotlin/zm/gov/moh/biodataservice/config/RedisConfig.kt
@@ -8,11 +8,9 @@ import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.data.redis.core.ReactiveRedisTemplate
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.RedisSerializationContext.RedisSerializationContextBuilder
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.data.redis.serializer.*
 import zm.gov.moh.biodataservice.model.FingerprintDao
+import java.util.UUID
 
 
 @Configuration
@@ -32,15 +30,14 @@ class RedisConfig {
 
     @Bean
     fun redisTemplate(connectionFactory: ReactiveRedisConnectionFactory): ReactiveRedisOperations<String, FingerprintDao> {
-        val valueSerializer: Jackson2JsonRedisSerializer<FingerprintDao> =
-            Jackson2JsonRedisSerializer(FingerprintDao::class.java)
+        val serializationContext: RedisSerializationContext<String, FingerprintDao> = RedisSerializationContext
+            .newSerializationContext<String, FingerprintDao>(StringRedisSerializer())
+            .key(StringRedisSerializer())
+            .value(Jackson2JsonRedisSerializer(FingerprintDao::class.java))
+            .hashKey(Jackson2JsonRedisSerializer(UUID::class.java))
+            .hashValue(Jackson2JsonRedisSerializer(FingerprintDao::class.java))
+            .build()
 
-        val builder: RedisSerializationContextBuilder<String, FingerprintDao> =
-            RedisSerializationContext.newSerializationContext(StringRedisSerializer())
-
-        val context: RedisSerializationContext<String, FingerprintDao> =
-            builder.value(valueSerializer).build()
-
-        return ReactiveRedisTemplate(connectionFactory, context)
+        return ReactiveRedisTemplate(connectionFactory, serializationContext)
     }
 }
--- bio-data-service/src/main/kotlin/zm/gov/moh/biodataservice/repository/BioDataRepository.kt
+++ bio-data-service/src/main/kotlin/zm/gov/moh/biodataservice/repository/BioDataRepository.kt
@@ -2,11 +2,13 @@ package zm.gov.moh.biodataservice.repository
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.ReactiveHashOperations
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import zm.gov.moh.biodataservice.model.FingerprintDao
+import zm.gov.moh.biodataservice.util.HashKey
 import java.util.UUID
 
 
@@ -15,26 +17,29 @@ class BioDataRepository(
     @Autowired
     private val reactiveOpts: ReactiveRedisOperations<String, FingerprintDao>
 ) {
+    private val hashOperations: ReactiveHashOperations<String, UUID, FingerprintDao> = reactiveOpts.opsForHash()
 
     companion object {
         private val logger = LoggerFactory.getLogger(BioDataRepository::class.java)
     }
 
     fun save(data: FingerprintDao): Mono<Boolean> {
-        return reactiveOpts.opsForValue().set(data.subjectId.toString(), data)
+        return hashOperations.put(HashKey.FINGER_PRINT_DATA, data.subjectId, data)
     }
 
     fun findAll(): Flux<FingerprintDao> {
-        return reactiveOpts.scan()
-            .flatMap(reactiveOpts.opsForValue()::get)
+        return hashOperations
+            .entries(HashKey.FINGER_PRINT_DATA)
+            .flatMap { Mono.just(it.value) }
     }
 
     fun findById(id: UUID): Mono<FingerprintDao>? {
-        return reactiveOpts.opsForValue().get(id.toString())
+        return hashOperations.get(HashKey.FINGER_PRINT_DATA, id)
     }
 
     fun delete(id: UUID): Mono<Boolean> {
         logger.warn("Deleting fingerprint images for client = {}", id)
-        return reactiveOpts.opsForValue().delete(id.toString())
+        val v = hashOperations.remove(HashKey.FINGER_PRINT_DATA, id)
+        return v.hasElement()
     }
 }
