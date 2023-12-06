313294fa2d518bc6ba9bdebc7af182dfb78644f8
Yengwe Tepula <lawrencetepula@gmail.com>
2023-12-06 12:30:02 +0200

    feat: return 404 when fingerprints don't match

---
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/model/AuxiliaryId.kt
@@ -0,0 +1,7 @@
+package zm.gov.moh.searchservice.model
+
+data class AuxiliaryId(
+    val auxiliaryId: Long,
+    val type: String?,
+    val value: String?
+)
--- search-service/src/main/kotlin/zm/gov/moh/searchservice/service/SearchService.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/service/SearchService.kt
@@ -3,6 +3,7 @@ package zm.gov.moh.searchservice.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import zm.gov.moh.searchservice.dto.SearchDTO
 import zm.gov.moh.searchservice.model.FingerprintDao
 import zm.gov.moh.searchservice.model.SearchPayload
 import zm.gov.moh.searchservice.model.Subject
@@ -15,22 +16,36 @@ class SearchService(
     @Autowired
     private val searchRepository: SearchRepository
 ) {
-    fun findSubject(subjectId: UUID): Mono<Subject> {
+    fun findSubject(subjectId: UUID): Mono<SearchDTO> {
         val subject = searchRepository.findSubjectBySubjectId(subjectId)
 
-        return Mono.just(subject)
+        val searchDTO = toSearchDTO(subject)
+
+        return Mono.just(searchDTO)
     }
 
     fun findFingerprint(searchPayload: SearchPayload): Mono<FingerprintDao> {
         val probeData = searchPayload.image
         val srcSystemId = searchPayload.sourceSystemId
 
-        val fingerprintFlux = searchRepository.findFingerprintBySrcSystemId(srcSystemId)
-
-        val fingerprint = fingerprintFlux.filter { fingerprintDao ->
-            fingerprintDao.data.any { Fingerprint.compareFingerprints(probeData, it.image) }
+        val fingerprint = searchRepository.findFingerprintBySrcSystemId(srcSystemId)
+            .filter { fingerprintDao ->
+                fingerprintDao.data.any { Fingerprint.compareFingerprints(probeData, it.image) }
         }.next()
 
         return fingerprint
+    }
+
+    private fun toSearchDTO(subject: Subject): SearchDTO {
+       val searchDTO = SearchDTO()
+
+        searchDTO.id = subject.id
+        searchDTO.firstName = subject.firstName
+        searchDTO.lastName = subject.lastName
+        searchDTO.sex = subject.sex
+        searchDTO.dateOfBirth = subject.dateOfBirth
+        searchDTO.auxiliaryIds = searchDTO.auxiliaryIds
+
+        return searchDTO
     }
 }
