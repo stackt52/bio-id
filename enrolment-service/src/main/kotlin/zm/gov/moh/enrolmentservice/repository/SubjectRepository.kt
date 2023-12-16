b9f3696fa3c786fe75a6327b12fa787eff55e884
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-12-16 18:42:51 +0200

    Updated SubjectRepository

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/repository/SubjectRepository.kt
+++ enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/repository/SubjectRepository.kt
@@ -1,9 +1,6 @@
 package zm.gov.moh.enrolmentservice.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+import org.springframework.data.r2dbc.repository.R2dbcRepository
 import zm.gov.moh.enrolmentservice.model.Subject
 import java.util.UUID
-@Repository
-interface SubjectRepository : JpaRepository<Subject, UUID> {
-}
+interface SubjectRepository : R2dbcRepository<Subject, UUID>
