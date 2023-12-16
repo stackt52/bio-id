b3f9628bc9e89f9e92d446faac631fc32a7c1aa1
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-12-16 18:33:55 +0200

    Updated the enrolment service to connect to the DB using the reactive APIs

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/service/EnrolmentService.kt
+++ enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/service/EnrolmentService.kt
@@ -1,82 +1,55 @@
 package zm.gov.moh.enrolmentservice.service
 
-import jakarta.transaction.Transactional
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.r2dbc.core.select
+import org.springframework.data.relational.core.query.Criteria.where
+import org.springframework.data.relational.core.query.Query.query
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import zm.gov.moh.enrolmentservice.client.FingerprintClient
 import zm.gov.moh.enrolmentservice.client.SearchClient
-import zm.gov.moh.enrolmentservice.controller.EnrolmentController
 import zm.gov.moh.enrolmentservice.model.FingerprintDao
 import zm.gov.moh.enrolmentservice.model.Subject
-import zm.gov.moh.enrolmentservice.repository.AuxiliaryRepository
-import zm.gov.moh.enrolmentservice.repository.SubjectRepository
 import java.util.*
 
 
 @Service
 class EnrolmentService(
-    @Autowired
-    private val searchClient: SearchClient,
-    @Autowired
-    private val fingerprintClient: FingerprintClient,
-    @Autowired
-    private val subjectRepository: SubjectRepository,
+        @Autowired
+        private val searchClient: SearchClient,
+        @Autowired
+        private val fingerprintClient: FingerprintClient,
+        @Autowired
+        private val template: R2dbcEntityTemplate,
 ) {
-
-    companion object {
-        val logger = LoggerFactory.getLogger(EnrolmentController::class.java)
-    }
-
-    @Transactional
     fun addSubject(subject: Subject): Mono<Subject> {
-        if (searchClient.search(subject.fingerprintData) == null) {
-            // We will need to changes the call to the subjectRepository.save for this is a
-            // blocking call and potentially might introduce thread starvation.
-            // Ideal implmentation would be to use R2DBC api to connect to relational datastore.
-            // See: https://github.com/pgjdbc/r2dbc-postgresql
-            return Mono.just(subjectRepository.save(subject))
-                .map { i -> FingerprintDao(i.id!!, subject.fingerprintData!!) }
-                .flatMap { i ->
-                    fingerprintClient.create(i).doOnNext { v ->
-                        if (v) {
-                            throw Exception("Error creating biometric data")
-                        }
-                    }
-                }.flatMap { i -> Mono.just(subject) }
-        } else {
-            throw Exception("Client Fingerprints already enrolled")
-        }
-    }
-
-    fun findAll(): List<Subject> {
-        throw NotImplementedError()
-    }
-
-    fun findById(id: UUID): Subject {
-        logger.info("Enrolment find: id={}", id)
-        val subject = subjectRepository.getReferenceById(id)
-        logger.info("Found: id={}", id)
-        return subject
-    }
-
-    fun updateById(subject: Subject): Subject {
-        // Only update subject data for now
-        return subjectRepository.save(subject)
-    }
-
-    @Transactional
-    fun deleteById(id: UUID) {
-        subjectRepository.deleteById(id)
-        fingerprintClient.delete(id)
-            .doOnError {
-                throw Exception("Error deleting biometric data")
-            }
-            .subscribe { result: Boolean? ->
-                if (!result!!) {
-                    throw Exception("Error deleting biometric data")
+        // Before enrolling new client perform a search to determine if fingerprint data already exists.
+        // only attempt registering new client when research returns Mono.empty().
+        return searchClient.search(subject.fingerprintData)
+                .hasElement()
+                .filter { i -> !i } // move to the next operator only if search returned empty
+                .flatMap { _ ->
+                    template.insert(subject)
+                            .flatMap { i -> fingerprintClient.create(FingerprintDao(i.id, i.fingerprintData)) }
+                            .flatMap { _ -> Mono.just(subject) }
                 }
-            }
+    }
+
+    fun findAll(): Flux<Subject> {
+        return template.select<Subject>().all()
+    }
+
+    fun findById(id: UUID): Mono<Subject> {
+        return template.selectOne(query(where("id").`is`(id)), Subject::class.java)
+    }
+
+    fun updateById(subject: Subject): Mono<Subject> {
+        return template.update(subject)
+    }
+
+    fun deleteById(id: UUID): Mono<Subject> {
+        return findById(id).flatMap { i -> template.delete(i) }
     }
 }
