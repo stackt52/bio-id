f3adc5f3e47642b7b646d01f474912684eb6ff8d
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-12-16 18:56:36 +0200

    Updated BioDataClient

--- search-service/src/main/kotlin/zm/gov/moh/searchservice/client/BioDataClient.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/client/BioDataClient.kt
@@ -1,14 +1,12 @@
 package zm.gov.moh.searchservice.client
 
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 import reactor.core.publisher.Flux
 import zm.gov.moh.searchservice.model.FingerprintDao
-import java.util.UUID
 
 @HttpExchange
 interface BioDataClient {
-    @GetExchange("/bio-data/src-system/{srcSystemId}")
-    fun findFingerprintDataBySrcSystemId(@PathVariable srcSystemId: UUID): Flux<FingerprintDao>
+    @GetExchange("/bio-data")
+    fun getAllBioData(): Flux<FingerprintDao>
 }
