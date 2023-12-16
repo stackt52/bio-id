c24b5c7c4326d55c5a441b94fa28586d7d182732
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-12-16 18:25:12 +0200

    Updated the enrolment controller to be returning reactive types

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/controller/EnrolmentController.kt
+++ enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/controller/EnrolmentController.kt
@@ -1,11 +1,11 @@
 package zm.gov.moh.enrolmentservice.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import zm.gov.moh.enrolmentservice.model.FingerprintDao
 import zm.gov.moh.enrolmentservice.model.Subject
 import zm.gov.moh.enrolmentservice.service.EnrolmentService
 import java.util.UUID
@@ -17,61 +17,62 @@ class EnrolmentController(
         private val enrolmentService: EnrolmentService
 ) {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(EnrolmentController::class.java)
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun add(@RequestBody subject: Subject): Mono<Subject> {
         try {
             subject.id = UUID.randomUUID()
             return enrolmentService.addSubject(subject)
-        } catch (e: IllegalArgumentException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
         } catch (e: Exception) {
-            throw ResponseStatusException(HttpStatus.CONFLICT, "Error occurred during transaction", e)
+            logger.error("Error occurred when enrolling subject: {}", e.stackTrace)
+            throw Throwable(e.message, e.cause)
         }
     }
 
     @GetMapping
-    fun findAll(): List<Subject> {
+    fun findAll(): Flux<Subject> {
         try {
             return enrolmentService.findAll()
-        } catch (e: IllegalArgumentException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        } catch (e: Exception) {
+            logger.error("Error occurred when getting subjects: {}", e.stackTrace)
+            throw Throwable(e.message, e.cause)
         }
     }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id: UUID): Subject {
+    fun findById(@PathVariable id: UUID): Mono<Subject> {
         try {
             return enrolmentService.findById(id)
-        } catch (e: IllegalArgumentException) {
-            EnrolmentService.logger.error("Enrolment find error", e)
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
         } catch (e: Exception) {
-            EnrolmentService.logger.error("Enrolment find error", e)
-            throw ResponseStatusException(HttpStatus.NOT_FOUND)
+            logger.error("Error occurred when retrieving subject with id = {}: {}", id, e.stackTrace)
+            throw Throwable(e.message, e.cause)
         }
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    fun updateById(@PathVariable id: UUID, @RequestBody subject: Subject): Subject {
+    fun updateById(@PathVariable id: UUID, @RequestBody subject: Subject): Mono<Subject> {
         try {
             subject.id = id
             return enrolmentService.updateById(subject)
-        } catch (e: IllegalArgumentException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        } catch (e: Exception) {
+            logger.error("Error occurred when updating subject = {}: {}", subject, e.stackTrace)
+            throw Throwable(e.message, e.cause)
         }
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun deleteById(@PathVariable id: String) {
+    fun deleteById(@PathVariable id: String): Mono<Subject> {
         try {
-            enrolmentService.deleteById(UUID.fromString(id))
-        } catch (e: IllegalArgumentException) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+            return enrolmentService.deleteById(UUID.fromString(id))
         } catch (e: Exception) {
-            throw ResponseStatusException(HttpStatus.CONFLICT, "Error occurred during transaction", e)
+            logger.error("Error occurred when deleting subject: {}", e.stackTrace)
+            throw Throwable(e.message, e.cause)
         }
     }
 }
