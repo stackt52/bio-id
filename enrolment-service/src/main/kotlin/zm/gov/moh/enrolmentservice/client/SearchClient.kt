f2950e4d6d00e504b9d9812aa4f38642a97c15eb
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-12-16 18:41:26 +0200

    Updated SearchClient

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/client/SearchClient.kt
+++ enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/client/SearchClient.kt
@@ -1,18 +1,15 @@
 package zm.gov.moh.enrolmentservice.client
 
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
+import reactor.core.publisher.Mono
 import zm.gov.moh.enrolmentservice.model.FingerprintData
 import zm.gov.moh.enrolmentservice.model.Subject
 
 @HttpExchange
 interface SearchClient {
-    @GetExchange("/search/{id}")
-    fun getById(@PathVariable id: String): List<FingerprintData>
 
     @PostExchange("/search")
-    fun search(@RequestBody searchPayload: MutableList<FingerprintData>?): Subject?
+    fun search(@RequestBody searchPayload: MutableList<FingerprintData>): Mono<Subject>
 }
