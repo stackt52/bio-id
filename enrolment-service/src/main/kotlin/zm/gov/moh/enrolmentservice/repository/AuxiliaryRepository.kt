e9175612827c904e539870d463ca02451213658d
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2023-12-16 18:20:58 +0200

    Deleted the AuxiliaryRepository.kt

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/repository/AuxiliaryRepository.kt
+++
@@ -1,8 +0,0 @@
-package zm.gov.moh.enrolmentservice.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import zm.gov.moh.enrolmentservice.model.AuxiliaryId
-@Repository
-interface AuxiliaryRepository : JpaRepository<AuxiliaryId, Long> {
-}
