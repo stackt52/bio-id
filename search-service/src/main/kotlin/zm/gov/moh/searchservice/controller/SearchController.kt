b4c4a4e0d4d6cce0239ee77a2e665a05aa396cb4
Yengwe Tepula <lawrencetepula@gmail.com>
2024-02-21 20:55:46 +0200

    feat: document apis using swagger

--- search-service/src/main/kotlin/zm/gov/moh/searchservice/controller/SearchController.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/controller/SearchController.kt
@@ -1,10 +1,17 @@
 package zm.gov.moh.searchservice.controller
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import zm.gov.moh.searchservice.dto.SearchDTO
@@ -13,11 +20,18 @@ import zm.gov.moh.searchservice.service.SearchService
 
 @RestController
 @RequestMapping("/search")
+@Api(tags = ["Search"], description = "Fingerprint search endpoint")
 class SearchController(
         @Autowired
         private val searchService: SearchService,
 ) {
-    @PostMapping
+    @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(
+        value = "Find a client using a given fingerprint image",
+        response = SearchDTO::class,
+        responseContainer = "List",
+    )
     fun search(@RequestBody searchPayload: SearchPayload): Mono<SearchDTO> {
         logger.info("Search: {}", searchPayload)
 
@@ -25,7 +39,7 @@ class SearchController(
             searchService.findSubject(it.subjectId)
         }.switchIfEmpty(
                 Mono.error(
-                        Throwable("No much found")
+                        Throwable("No match found")
                 )
         )
     }
