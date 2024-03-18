86d06f0b3c800ff2c35881f6feff8e7c370ffe0e
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2024-03-18 15:59:52 +0200

    Updated BioDataClient

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/client/BioDataClient.kt
+++ enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/client/BioDataClient.kt
@@ -1,30 +1,27 @@
 package zm.gov.moh.enrolmentservice.client
 
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.service.annotation.DeleteExchange
 import org.springframework.web.service.annotation.GetExchange
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
 import org.springframework.web.service.annotation.PutExchange
-import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import zm.gov.moh.enrolmentservice.model.FingerprintDao
-import zm.gov.moh.enrolmentservice.model.FingerprintData
+import zm.gov.moh.enrolmentservice.model.FingerprintDTO
 import java.util.*
 
 @HttpExchange
-interface FingerprintClient {
+interface BioDataClient {
 
     @PostExchange("/bio-data")
-    fun create(@RequestBody fingerPrint: FingerprintDao): Mono<FingerprintDao>
+    fun create(@RequestBody fingerPrint: FingerprintDTO): Mono<FingerprintDTO>
 
     @GetExchange("/bio-data/{subjectId}")
-    fun findById(@PathVariable subjectId: UUID): Mono<FingerprintDao>
+    fun findById(@PathVariable subjectId: UUID): Mono<FingerprintDTO>
 
     @PutExchange("/bio-data/{subjectId}")
-    fun update(@RequestBody fingerPrint: FingerprintDao): Mono<FingerprintDao>
+    fun update(@RequestBody fingerPrint: FingerprintDTO): Mono<FingerprintDTO>
 
     @DeleteExchange("/bio-data/{subjectId}")
-    fun delete(@PathVariable subjectId: UUID): Mono<FingerprintDao>
+    fun delete(@PathVariable subjectId: UUID): Mono<FingerprintDTO>
 }
