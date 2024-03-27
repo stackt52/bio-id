92c7af4cc012068746403f70502ee69f36938fab
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2024-03-27 16:47:52 +0200

    Updated ClientDTO in enrolment-service

--- enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/model/ClientDTO.kt
+++ enrolment-service/src/main/kotlin/zm/gov/moh/enrolmentservice/model/ClientDTO.kt
@@ -10,9 +10,8 @@ data class ClientDTO(
     val lastName: String,
     val sex: Sex,
     val dateOfBirth: LocalDate,
+    @Transient
     var fingerprintImages: MutableList<FingerprintImageDTO> = mutableListOf(),
     var auxiliaryIds: MutableList<AuxiliaryIdDTO> = mutableListOf(),
     var newClient: Boolean = false
-) {
-    var sourceSystemId: String = ""
-}
+)
