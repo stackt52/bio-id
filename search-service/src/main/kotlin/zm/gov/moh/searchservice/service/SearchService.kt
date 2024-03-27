55f79c3be04ce3172bcaa4f4d3907d1869f6b1bd
Terence Kaite <136319286+Stackd52@users.noreply.github.com>
2024-03-27 16:44:04 +0200

    Added enrolment web client for access client details in search-service

--- search-service/src/main/kotlin/zm/gov/moh/searchservice/service/SearchService.kt
+++ search-service/src/main/kotlin/zm/gov/moh/searchservice/service/SearchService.kt
@@ -9,13 +9,22 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import zm.gov.moh.searchservice.client.BioDataClient
+import zm.gov.moh.searchservice.client.EnrolmentClient
 import zm.gov.moh.searchservice.model.*
 
 @Service
 class SearchService(
     @Autowired
     private val bioDataClient: BioDataClient,
+    @Autowired
+    private val enrolmentClient: EnrolmentClient
 ) {
+
+    fun searchEnrolledClient(image: ByteArray): Mono<ClientDTO> {
+        return identify(image).flatMap { i ->
+            enrolmentClient.findById(i.subjectId)
+        }
+    }
 
     fun identifyAny(searchPayload: List<FingerprintImageDTO>): Mono<MatchScore> {
         /**
@@ -23,14 +32,14 @@ class SearchService(
          * considering that we are running it on async streams.
          */
         return Flux.fromIterable(searchPayload)
-            .flatMap { i -> identify(i) }
+            .flatMap { i -> identify(i.image) }
             .take(1) // receive one value and shutdown the subscription
             .toMono()
     }
 
-    fun identify(fingerprintImage: FingerprintImageDTO): Mono<MatchScore> {
+    private fun identify(image: ByteArray): Mono<MatchScore> {
         val threshold = 120.0
-        val fImage = FingerprintImage(fingerprintImage.image)
+        val fImage = FingerprintImage(image)
         val fTemplate = FingerprintTemplate(fImage)
         val matcher = FingerprintMatcher(fTemplate) // this is an expensive operation
 
