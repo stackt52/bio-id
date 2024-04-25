dff650d35b06db4889ba933f7b99ec36ee0ac821
Terence Kaite <terrencekaite@gmail.com>
2024-04-25 18:46:54 +0200

    macos dns resolver

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.109.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
