2898b65f9d1297c717a4a7c361ddaead995aa5f7
Terence Kaite <terrencekaite@gmail.com>
2024-04-25 18:32:04 +0200

    Removed the custom authentication manager

--- identity-service/src/main/kotlin/zm/gov/moh/identityservice/config/SecurityConfig.kt
+++ identity-service/src/main/kotlin/zm/gov/moh/identityservice/config/SecurityConfig.kt
@@ -1,25 +1,17 @@
 package zm.gov.moh.identityservice.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.ReactiveAuthenticationManager
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.Authentication
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
-import zm.gov.moh.identityservice.service.UserDetailsService
 
 
 @Configuration
 @EnableWebFluxSecurity
-class SecurityConfig(
-    @Autowired
-    private val userDetailsService: UserDetailsService
-) {
+class SecurityConfig {
 
     @Bean
     fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -34,16 +26,5 @@ class SecurityConfig(
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
-    }
-
-    @Bean
-    fun reactiveAuthenticationManager(): ReactiveAuthenticationManager {
-        return ReactiveAuthenticationManager { authentication: Authentication ->
-            val authenticator = UserDetailsRepositoryReactiveAuthenticationManager(
-                userDetailsService
-            )
-            authenticator.setPasswordEncoder(passwordEncoder())
-            authenticator.authenticate(authentication)
-        }
     }
 }
